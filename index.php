8285072414de8a81e76145155abe97d7c5171077
AbulHossain/Adnan <alhossain.a92@gmail.com>
2021-11-22 22:42:03 -0800

    calculator app using php switch

--- index.php
+++ index.php
@@ -23,8 +23,6 @@ echo sum(50,10);
 <!-- 2.create peramiters name and age and print the value. -->
 
 <?php
-
-
 function peramiters($name, $age){
 	echo "my name is"." ".$name."i am ".$age." "."years old";
 }
@@ -33,6 +31,105 @@ peramiters('adnan',22);
 ?>
 
 
+
+<!-- create calculator using php switch -->
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1 class="text-center">Simple Calculator</h1>
+
+
+<div class="row">
+  <div class="col-sm-10 m-auto">
+    <div class="card">
+      <div class="card-body">
+        <form>
+
+  <div class="row">
+    <div class="col">
+      <input type="text" name="fnum" class="form-control" placeholder="First number">
+    </div>
+    <div class="col">
+      <input type="text" name="snum" class="form-control" placeholder="second number">
+    </div>
+    <div class="col" >
+     <select type="text" name="operators" class="form-control" >
+     	<option>none</option>
+     	<option>Add</option>
+     	<option>Subtract</option>
+     	<option>Multiply</option>
+     	<option>Division</option>
+     	
+     </select>
+    </div>
+  </div><br>
+  <button class="btn btn-success" name="submit" type="submit">Calculate</button>
+</form><br>
+<h3>The Answer is:</h3>
+<?php
+if(isset($_GET['submit'])) {
+	$fnum=$_GET['fnum'];
+	$snum=$_GET['snum'];
+	$operators=$_GET['operators'];
+
+	switch ($operators) {
+		case 'none':
+			echo "plese select any operrator";
+			break;
+			case 'Add':
+			echo $fnum+$snum;
+			break;
+			case 'Subtract':
+			echo  $fnum-$snum;;
+			break;
+			case 'Multiply':
+			echo  $fnum*$snum;;
+			break;
+			case 'Division':
+			echo $fnum/$snum;;
+			break;
+	
+	}
+}
+?>
+      </div>
+    </div>
+  </div>
+ 
+</div>
+
+
+
+
+
+
+
+<?php
+
+?>
+
+
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
 
 
 

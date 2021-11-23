a6d97edf1800c0532e7210237fe8fe25aebf30af
AbulHossain/Adnan <alhossain.a92@gmail.com>
2021-11-22 21:19:09 -0800

    first task to do complete

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+<!-- .crate a function and sum betwen two number and print the value -->
+
 
 <?php
-echo "hello world";
+function sum(int $x, int $y){
+
+	$z=$x+$y;
+	return $z;
+
+} 
+echo sum(50,10);
 ?>
+
+</body>
+</html>
